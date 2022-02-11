431355a703f060b710a58841f7b7636a883d7419
Matteo D'Alessandro <dalmatteo@gmail.com>
2022-02-11 09:57:47 +0100

    add streamlit sidebar

--- project_streamlit.py
+++ project_streamlit.py
@@ -9,18 +9,12 @@ oscar_df = pd.read_csv('the_oscar_award.csv')
 metadata_df = pd.read_csv('movies_metadata.csv')
 
 oscar_df = oscar_df.drop(['year_ceremony','ceremony'], axis=1)
-
-mask = (oscar_df.category == 'HONORARY AWARD') | \
-       (oscar_df.category == 'SPECIAL AWARD') | \
-       (oscar_df.category == 'IRVING G. THALBERG MEMORIAL AWARD') | \
-       (oscar_df.category == 'JEAN HERSHOLT HUMANITARIAN AWARD') | \
-       (oscar_df.category == 'SPECIAL ACHIEVEMENT AWARD') | \
-       (oscar_df.category == 'HONORARY FOREIGN LANGUAGE FILM AWARD') | \
-       (oscar_df.category == 'SPECIAL FOREIGN LANGUAGE FILM AWARD')
-
-oscar_df = oscar_df[~mask]
-
 oscar_df = oscar_df.dropna(subset=['film'])
 
-st.text('Fixed width text')
-st.text('Fixed width text')
+st.header('The Oscars analysis')
+
+a = st.sidebar.radio('Sections:', ['Data cleaning', 'Academy Award fun facts', 'Other plots', 'Predictive model'])
+
+if a == 'Predictive model':
+       st.subheader('Predictive Model')
+       st.write('')
